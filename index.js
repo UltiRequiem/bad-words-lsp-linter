3d82fa6fefac4296e34ad111c0cf23fd56dc4930
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-28 17:15:09 -0500

    :wrench: Add Bin Propety to package.json

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ const blacklist = require("./badWords.js");
 
 const getBlacklisted = (text) => {
   const regex = new RegExp(`\\b(${blacklist.join("|")})\\b`, "gi");
+
   const results = [];
   regex.lastIndex = 0;
+
   while ((matches = regex.exec(text)) && results.length < 100) {
     results.push({
       value: matches[0],
       index: matches.index,
     });
   }
+
   return results;
 };
 
@@ -29,8 +32,8 @@ const blacklistToDiagnostic =
       start: textDocument.positionAt(index),
       end: textDocument.positionAt(index + value.length),
     },
-    message: `${value} is blacklisted.`,
-    source: "Blacklister",
+    message: `${value} is a bad word!`,
+    source: "Bad Words Linter",
   });
 
 const getDiagnostics = (textDocument) =>
