22943608564fc0aa743f36375e9d43f179c3cba9
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-28 17:34:37 -0500

    :bug: Fix Bug with List of Bad Words

--- badWords.js
+++ badWords.js
@@ -2,10 +2,10 @@ const fs = require("node:fs");
 
 function getBadWords() {
   return fs
-    .readFileSync("./ListOfBadWords.txt", { encoding: "utf8" })
+    .readFileSync(`${__dirname}/ListOfBadWords.txt`, { encoding: "utf8" })
     .split("\n");
 }
 
-const badWords = getBadWords()
+const badWords = getBadWords();
 
 module.exports = badWords;
